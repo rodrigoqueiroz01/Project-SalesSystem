842f4e21aa83b7e5ab1467ce394afc477c26e3ce
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2022-07-14 11:40:28 -0300

    Update Project

--- src/main/java/io/github/rodrigoqueiroz01/http/controller/ClientController.java
+++ src/main/java/io/github/rodrigoqueiroz01/http/controller/ClientController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/vendas")
+@RequestMapping("/sales")
 @AllArgsConstructor
 @Validated
 public class ClientController {
